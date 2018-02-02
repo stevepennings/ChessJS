5f11ff2f12ccc028e65b094002acded6f325d2ec
Steve Pennings <stevepennings@live.nl>
2018-02-02 14:58:58 +0100

    styling done, chessboard finished

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function createBoard() {
 function createPositionsGrid() {
     // Board reference
     var positions = document.getElementById("positions");
+    var colorSwitch = 1;
     // Nested loop
     for (var row = 0; row < 8; row++) {
         var vPositions = document.createElement("tr");
@@ -18,22 +19,48 @@ function createPositionsGrid() {
             position.style.backgroundColor = "white";
             position.positionId = positionId++;
 
-            if(position.positionId % 2 == 0) {
-                position.style.backgroundColor = "black";
+            // Change background color on odd numbers to create a chess table
+            if (position.positionId % 8 == 0) {
+                if (colorSwitch === 1) {
+                    colorSwitch = 0;
+                } else {
+                    colorSwitch = 1;
+                }
+            }
+
+            if(position.positionId % 2 == colorSwitch) {
+                position.style.backgroundColor = "#f5f5f5";
+            } else {
+                position.style.backgroundColor = "#c2c2c2";
             }
 
             vPositions.appendChild(position);
             // Print position id's for testing
-            position.innerHTML = positionId;
+            // position.innerHTML = positionId;
         }
         positions.appendChild(vPositions);
     }   
 }
 
 function createHorizontalIndex() {
+    var horizontalPositions = document.getElementById("horizontalPositions");
+    var hContent = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
     // The letters from A-H of a chessboard
+    for (var column = 0; column < hContent.length; column++) {
+        var hIndex = document.createElement("td");
+        // Print position id's for testing
+        hIndex.innerHTML = hContent[column];
+        horizontalPositions.appendChild(hIndex);
+    }   
 }
 
 function createVerticalIndex() {
-    // The numbers from 1-8 of a chessboard
+    var verticalPositions = document.getElementById("verticalPositions");
+    // The letters from A-H of a chessboard
+    for (var column = 0; column < 8; column++) {
+        var vIndex = document.createElement("tr");
+        // Print position id's for testing
+        vIndex.innerHTML = column;
+        verticalPositions.appendChild(vIndex);
+    }  
 }
